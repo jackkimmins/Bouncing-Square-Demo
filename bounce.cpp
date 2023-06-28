d4a57063b747050b0c6dc8ff8bac59d6fc578ae9
jackkimmins <jackkimmins@outlook.com>
2023-06-28 10:11:46 +0000

    Updates to the code

--- bounce.cpp
+++ bounce.cpp
@@ -7,60 +7,69 @@
 #define HEIGHT 600
 #define BOX_SIZE 40
 #define SPEED 2
+#define MAX_COLOR_VALUE 256
 
 static SDL_Window* window = nullptr;
 static SDL_Renderer* renderer = nullptr;
 static int directionX;
 static int directionY;
 static SDL_Rect square = { .x = WIDTH / 2, .y = HEIGHT / 2, .w = BOX_SIZE, .h = BOX_SIZE };
-static int r = 255, g = 255, b = 255; // initial color is white
+static int colors[3] = {255, 255, 255};
 
-void render() {
+void ChangeColour()
+{
+    for (int i = 0; i < 3; i++) colors[i] = rand() % MAX_COLOR_VALUE;
+}
+
+void Render()
+{
     // Clear screen
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     // Draw square
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+    SDL_SetRenderDrawColor(renderer, colors[0], colors[1], colors[2], 255);
     SDL_RenderFillRect(renderer, &square);
 
     // Update screen
     SDL_RenderPresent(renderer);
 }
 
-void update() {
-    // Update square position
+void Update()
+{
+    // Update square position.
     square.x += directionX;
     square.y += directionY;
 
-    // Check for collision with window borders
-    if (square.x + square.w > WIDTH || square.x < 0) {
+    // Check for collision with window borders and change colour.
+    if (square.x + square.w > WIDTH || square.x < 0)
+    {
         directionX = -directionX;
-        r = rand() % 256;
-        g = rand() % 256;
-        b = rand() % 256;
+        ChangeColour();
     }
-    if (square.y + square.h > HEIGHT || square.y < 0) {
+
+    if (square.y + square.h > HEIGHT || square.y < 0)
+    {
         directionY = -directionY;
-        r = rand() % 256;
-        g = rand() % 256;
-        b = rand() % 256;
+        ChangeColour();
     }
 }
 
 void mainloop() {
-    update();
-    render();
+    Update();
+    Render();
 }
 
-int main() {
-    srand(time(NULL)); // Seed the random number generator
+int main()
+{
+    // Seed the random number generator.
+    srand((unsigned int)time(NULL));
 
-    // Random initial directions
+    // Random initial directions.
     directionX = (rand() % 2 ? 1 : -1) * SPEED;
     directionY = (rand() % 2 ? 1 : -1) * SPEED;
 
-    // Initialize SDL
+    // Initialise SDL
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("Bouncing Square Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, 0);
